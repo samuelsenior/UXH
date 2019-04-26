1972a446682eb95f824cabd70d795821d74f9ca1
Samuel Senior <s.m.senior@soton.ac.uk>
2019-04-27 00:55:11 +0100

    Bug fixes

--- XNLO/lib/XNLO.hpp
+++ XNLO/lib/XNLO.hpp
@@ -1,5 +1,8 @@
 #include "../../Eigen/Dense"
 
+#include "../maths/maths_textbook.hpp"
+#include "../physics/physics_textbook.hpp"
+
 using namespace Eigen;
 
 namespace XNLO {
@@ -11,6 +14,7 @@ namespace XNLO {
 		ArrayXXcd wavefunction;
 	};
 
-	Result XNLO(ArrayXXcd A_w_active, ArrayXd w_active, int w_active_min_index_UPPE, std::string print="minimum");
+	Result XNLO(ArrayXXcd A_w_active, ArrayXd w_active, int w_active_min_index_UPPE,
+				maths_textbook& maths, physics_textbook& physics, std::string print="minimum");
 
 } // XNLO namespace
